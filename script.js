5c1b073eeb629faa6666cfa2c9687af4a6b22cf8
Artem Titov <TETOV1158@mail.ru>
2025-04-13 17:44:15 +0300

    "+ Timer approach"

--- script.js
+++ script.js
@@ -2293,3 +2293,54 @@ const now = new Date();
 // alert(`Цикл отработал за ${end - start} миллисекунд`);
 
 // console.log(now);
+
+
+// Timer
+
+// const deadLine = '2025-05-11';
+
+// function getTimeRemaining(endTime) {
+//     const t = Date.parse(endTime) - Date.parse(new Date());
+//     const days = Math.floor(t / (1000 * 60 * 60 * 24));
+//     const hours = Math.floor((t / (1000 * 60 * 60) % 24));
+//     const minutes = Math.floor((t / 1000 / 60) % 60);
+//     const sec = Math.floor((t / 1000) % 60);
+
+//     return {
+//         'total': t,
+//         'days': days,
+//         'hours': hours,
+//         'minutes': minutes,
+//         'seconds': sec
+//     };
+// }
+
+// function setClock(selector, endTime) {
+//     const timer = document.querySelector(selector),
+//         days = timer.querySelector('#days'),
+//         hours = timer.querySelector('#hours'),
+//         minutes = timer.querySelector('#minutes'),
+//         seconds = timer.querySelector('#seconds'),
+//         timeInterval = setInterval(updateClock, 1000);
+
+//     updateClock();
+
+//     function updateClock() {
+//         const t = getTimeRemaining(endTime);
+
+//         days.innerHTML = (t.days < 10 ? `0${t.days}` : t.days);
+//         //days.innerHTML = t.days;
+//         hours.textContent = (t.hours < 10 ? `0${t.hours}` : t.hours);
+//         //hours.innerHTML = (t.hours);
+//         minutes.innerHTML = (t.minutes < 10 ? `0${t.minutes}` : t.minutes);
+//         //minutes.textContent = (t.minutes);
+//         seconds.innerHTML = (t.seconds);
+
+
+//         if (t.total <= 0) {
+//             clearInterval(timeInterval);
+//         }
+//     }
+// }
+
+// setClock('.timer', deadLine);
